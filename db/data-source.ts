760b9175136d50d2911b44378c0af58041e74e70
Khoa Nguyen <khoanguyen.270799@gmail.com>
2024-03-15 15:20:30 +0700

    first commit

---
+++ db/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSourceOptions, DataSource } from "typeorm";
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "blog",
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+    synchronize: false
+}
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
